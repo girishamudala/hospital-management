b304fd2a70f06ff4be907b882dcf9012397ef571
sandytony <vayalpatisandeep@gmail.com>
2021-06-22 22:57:34 +0530

    models_rm updated

--- hospital_management/core/models/models_rm.py
+++ hospital_management/core/models/models_rm.py
@@ -2,14 +2,18 @@ from django.db import models
 
 # Create your models here.
 class Test(models.Model):
-    id = models.IntegerField()
     test_name = models.CharField(max_length=50)
     test_description = models.CharField(max_length=1000)
     price = models.IntegerField()
 
+    def __str__(self):
+        return self.test_name
 
 class Medicine(models.Model):
-    id = models.IntegerField()
     medicine_name = models.CharField(max_length=50)
     medicine_description = models.CharField(max_length=1000)
     price = models.IntegerField()
+
+    def __str__(self):
+        return self.medicine_name
+
