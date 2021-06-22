da6f46c162779146591ed81462e7d7bcca7cd114
gopin03 <gopin03@gmail.com>
2021-06-22 22:14:08 +0530

    models updated

---
+++ hospital_management/core/models/models_nc.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+
+class Departmnet=(models.Model):
+    id=models.IntegerField(null=True)
+    name=models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
+
+
+class Doctor=(models.Model):
+    id=models.IntegerField(null=True)
+    name=models.CharField(max_length=100)
+    experiance=models.IntegerField(null=True)
+    specialization=models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.specialization
